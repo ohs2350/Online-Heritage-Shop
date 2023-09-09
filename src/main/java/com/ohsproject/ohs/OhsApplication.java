a916fa9e83bf745a835d6a2280537507050a92bf
ohs <ohs9207@gmail.com>
2023-09-09 19:38:05 +0900

    chore : 프로젝트 생성

---
+++ src/main/java/com/ohsproject/ohs/OhsApplication.java
@@ -0,0 +1,13 @@
+package com.ohsproject.ohs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohsproject/ohs/OhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohsproject.ohs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
