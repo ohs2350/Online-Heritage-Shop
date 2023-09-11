f1af4c193813fb0a02277fddd6fe542199faf54a
ohs <ohs9207@gmail.com>
2023-09-11 19:02:13 +0900

    feat : 각 엔티티별 repository 생성

---
+++ src/main/java/com/ohsproject/ohs/orderDetail/domain/OrderDetailRepository.java
@@ -0,0 +1,6 @@
+package com.ohsproject.ohs.orderDetail.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+}
