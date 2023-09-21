9ef1846966eda01a75fe51ec6ea5fd59db94d82d
ohs <ohs9207@gmail.com>
2023-09-21 21:05:53 +0900

    feat : 로그인 로직 구현

---
+++ src/main/java/com/ohsproject/ohs/member/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.ohsproject.ohs.member.service;
+
+import com.ohsproject.ohs.member.domain.Member;
+import com.ohsproject.ohs.member.domain.MemberRepository;
+import com.ohsproject.ohs.member.dto.request.MemberLoginDto;
+import com.ohsproject.ohs.member.exception.MemberNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void login(MemberLoginDto memberLoginDto, HttpSession httpSession) {
+        Member member = findMemberById(memberLoginDto.getId());
+        String sessionId = UUID.randomUUID().toString();
+
+        httpSession.setAttribute(sessionId, member);
+    }
+
+    private Member findMemberById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(MemberNotFoundException::new);
+    }
+}
