14daf8538e6a6eff30d6d5ecfc8ce9287181bbe8
ohs <ohs9207@gmail.com>
2023-09-21 20:30:47 +0900

    test : 로그인 로직 및 api에 대한 테스트 추가

---
+++ src/test/java/com/ohsproject/ohs/member/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package com.ohsproject.ohs.member;
+
+import com.ohsproject.ohs.member.domain.Member;
+import com.ohsproject.ohs.member.domain.MemberRepository;
+import com.ohsproject.ohs.member.dto.request.MemberLoginDto;
+import com.ohsproject.ohs.member.exception.MemberNotFoundException;
+import com.ohsproject.ohs.member.service.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpSession;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService memberService;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    private static final long MEMBER_ID = 1L;
+
+    @Test
+    @DisplayName("로그인 성공")
+    void login() {
+        // given
+        Member member = createSampleMember();
+        MemberLoginDto memberLoginDto = createSampleMemberLoginDto();
+        MockHttpSession session = new MockHttpSession();
+        when(memberRepository.findById(1L)).thenReturn(Optional.of(member));
+
+        // when
+        memberService.login(memberLoginDto, session);
+
+        // then
+        verify(memberRepository, times(1)).findById(anyLong());
+    }
+
+    @Test
+    @DisplayName("잘못된 아이디로 로그인하는 경우")
+    void login_MemberNotFound() {
+        // given
+        MemberLoginDto memberLoginDto = createSampleMemberLoginDto();
+        MockHttpSession session = new MockHttpSession();
+        when(memberRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // when, then
+        assertThrows(MemberNotFoundException.class, () -> memberService.login(memberLoginDto, session));
+    }
+
+
+    private Member createSampleMember() {
+        return new Member(MEMBER_ID, "test");
+    }
+
+    private MemberLoginDto createSampleMemberLoginDto() {
+        return new MemberLoginDto(MEMBER_ID);
+    }
+
+}
