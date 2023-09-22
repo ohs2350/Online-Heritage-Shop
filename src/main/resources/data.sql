0722c139636e9b3371f5ef8ac4c3f6fd07b2eaee
ohs <ohs9207@gmail.com>
2023-09-22 21:01:42 +0900

    chore : 개발용 데이터 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member(member_id, name) values(1, 'test');
+insert into product(product_id, price, stock, hit) values(1, 1000, 5, 1);
+insert into product(product_id, price, stock, hit) values(2, 1500, 10, 2);
