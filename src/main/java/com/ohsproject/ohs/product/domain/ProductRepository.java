96cab77f7165e4c29fe42ae6a50bfce18418c713
ohs <ohs9207@gmail.com>
2023-10-21 16:14:58 +0900

    feat : 상품 조회 시 비관적 락 적용

--- src/main/java/com/ohsproject/ohs/product/domain/ProductRepository.java
+++ src/main/java/com/ohsproject/ohs/product/domain/ProductRepository.java
@@ -1,6 +1,14 @@
 package com.ohsproject.ohs.product.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import javax.persistence.LockModeType;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Override
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Product> findById(Long id);
 }
