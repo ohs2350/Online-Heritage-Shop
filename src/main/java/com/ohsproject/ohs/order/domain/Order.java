8e3398178e4b0e1deb68e2e2759dad9a60c5f2e7
ohs <ohs9207@gmail.com>
2023-11-02 18:47:17 +0900

    feat : Order 엔티티 필드 추가

--- src/main/java/com/ohsproject/ohs/order/domain/Order.java
+++ src/main/java/com/ohsproject/ohs/order/domain/Order.java
@@ -1,19 +1,21 @@
 package com.ohsproject.ohs.order.domain;
 
 import com.ohsproject.ohs.member.domain.Member;
-import com.ohsproject.ohs.orderDetail.domain.OrderDetail;
+import com.ohsproject.ohs.order.exception.OrderNotValidException;
 import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 @Getter
 @Table(name = "orders")
 @Builder
 public class Order {
+
+    private static final int ORDER_VALID_TIME = 15;
+
     @Id @Column(name = "order_id")
     @GeneratedValue
     private Long id;
@@ -28,14 +30,32 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
     protected Order() {
 
     }
 
-    private Order(Long id, LocalDateTime orderDate, int amount, Member member) {
+    private Order(Long id, LocalDateTime orderDate, int amount, Member member, OrderStatus status) {
         this.id = id;
         this.orderDate = orderDate;
         this.amount = amount;
         this.member = member;
+        this.status = status;
+    }
+
+    public void changeStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void validateOrder() {
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isAfter(this.orderDate.plusMinutes(ORDER_VALID_TIME)) ) {
+            throw new OrderNotValidException();
+        }
+        if (!this.status.equals(OrderStatus.ORDER)) {
+            throw new OrderNotValidException();
+        }
     }
 }
