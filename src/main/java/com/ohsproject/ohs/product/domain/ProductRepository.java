67dc5dfbf1ded9e74da5b77122ed48918f0a3afd
ohs <ohs9207@gmail.com>
2023-11-02 18:57:41 +0900

    feat : 재고 정보량에 대해 update 쿼리 적용

--- src/main/java/com/ohsproject/ohs/product/domain/ProductRepository.java
+++ src/main/java/com/ohsproject/ohs/product/domain/ProductRepository.java
@@ -1,14 +1,12 @@
 package com.ohsproject.ohs.product.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-
-import javax.persistence.LockModeType;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Override
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<Product> findById(Long id);
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query(value = "update Product p set p.stock = p.stock - :orderQty where p.id = :productId and p.stock >= :orderQty")
+    int decreaseProductStock(Long productId, int orderQty);
 }
